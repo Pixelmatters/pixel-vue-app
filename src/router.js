95926165170329dd639fc38b71f9ab7333e093fc
Rui Saraiva <ruisaraiva19@gmail.com>
2019-02-04 22:30:36 +0000

    Step 10 - Added /posts/:postId route

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Posts from './views/Posts.vue'
+import Post from './views/Post.vue'
 
 Vue.use(Router)
 
@@ -18,6 +19,11 @@ export default new Router({
       path: '/posts',
       name: 'posts',
       component: Posts
+    },
+    {
+      path: '/posts/:postId',
+      name: 'post',
+      component: Post
     },
     {
       path: '/about',
